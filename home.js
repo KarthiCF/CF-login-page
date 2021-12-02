bf6a4ef92995892d29492faf41ef5ad49c03e69c
TimothyDal <93418745+TimothyDal@users.noreply.github.com>
2021-12-02 16:57:09 +0530

    update in code

---
+++ home.js
@@ -0,0 +1,20 @@
+const email = document.getElementById("check-email");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error")
+
+
+// var email_error = document.getElementById('email_error');
+// var pass_error = document.getElementById('pass_error');
+
+
+
+function credentials_fill(){
+    if(email.value === '' || email.value === null){
+        alert("Please enter email")
+    }
+    if(password.value === '' || password.value === null){
+        alert("Please enter password")
+    }
+
+}

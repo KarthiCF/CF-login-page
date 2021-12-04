e1d5a7988c87f407b45fe64d091ed4e9c4d31fef
TimothyDal <93418745+TimothyDal@users.noreply.github.com>
2021-12-04 09:20:33 +0530

    login and sign up

--- home.js
+++ home.js
@@ -1,20 +1,88 @@
-const email = document.getElementById("check-email");
-const password = document.getElementById("password");
-const form = document.getElementById("form");
-const errorElement = document.getElementById("error")
+/*Js code for login page */
+
+// const email = document.getElementById("check-email");
+// const password = document.getElementById("password");
+// const form = document.getElementById("form");
+// const errorElement = document.getElementById("error")
 
 
-// var email_error = document.getElementById('email_error');
-// var pass_error = document.getElementById('pass_error');
+// function credentials_fill(){
+//     let email = document.getElementById("check-email");
+//     let password1 = document.getElementById("password1");
+//     var e1 = document.getElementById("e1")
+//     var e2 = document.getElementById("e2")
+//     if(password1.value < 8){
+//         e2.style.display="block"
+//     }
+//     else{
+//         e2.style.display="none"
+//     }
+
+// }
 
 
 
-function credentials_fill(){
-    if(email.value === '' || email.value === null){
-        alert("Please enter email")
+
+
+/* Js code for sign up page */
+
+
+
+(function() {   //This function executesd the validation of the sign up page
+    'use strict';
+    window.addEventListener('load', function() {
+      var forms = document.getElementsByClassName('needs-validation');
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
+
+// function passcheck(){
+//     let Signin_Pass = document.getElementById("SignPass")
+//     let Signin_Pass_Confirm = document.getElementById("SignPassConfirm")
+//     // var e1 = document.getElementById("e1")
+//     if(Signin_Pass.value === Signin_Pass_Confirm.value ){
+//         e1.style.display = "none"
+//     }
+    
+
+//     else{
+//         e1.style.display = "block"
+//     }
+// }
+
+
+
+// function Validate(){
+//     var e1 = document.getElementById("e1")
+//     var e2 = document.getElementById("e2")
+//     if(Signin_Pass.value.match(/[0-9]/)){
+//         e2.style.display = "none"
+//     }
+//     else{
+        
+//     }
+    
+// }
+
+
+
+var passcheck = function() {
+    
+    if (document.getElementById('SignPass').value ==
+      document.getElementById('SignPassConfirm').value)  {
+      document.getElementById('e1').style.display="none";
+      }
+    else{
+      document.getElementById('e1').style.display="block";
+    
     }
-    else if(password.value === '' || password.value === null){
-        alert("Please enter password")
-    }
-
-}
+  }

9fe1bbc00aea61f3c7da7ce91e1f37b8f9d4dfec
TimothyDal <93418745+TimothyDal@users.noreply.github.com>
2021-12-09 13:46:37 +0530

    filter by name

--- home.js
+++ home.js
@@ -136,6 +136,8 @@ function searchBox(value, data){
     if (name.includes(value)){
       filterData.push(data[i])
     }
+    if (name.includes(value)){
+      filterData.push(data[i])
   }
   return filterData
 }
